1260361d3c1af65eef8da32a84bb9f790c255b96
Apurva Sawant <apurvasawant45@gmail.com>
2022-02-04 18:36:40 +0530

    add: colors and typography page

--- js/main.js
+++ js/main.js
@@ -9,3 +9,24 @@ document.addEventListener('DOMContentLoaded', (event) => {
       hljs.highlightElement(el)
     })
 })
+
+const github = document.querySelector(".fa-github");
+
+const hamburger = document.querySelector(".my-hamburger");
+const scrollbar = document.querySelector('.scrollbar')
+const main = document.querySelector('.main')
+
+github.addEventListener("click", ()=>{
+  hamburger.style.display="none";
+})
+
+hamburger.addEventListener("click", () => {
+    scrollbar.style.display="block"
+    main.style.opacity = 0.8;
+})
+
+
+main.addEventListener("click",()=>{
+  scrollbar.style.display="none"
+  main.style.opacity = 1;
+});
