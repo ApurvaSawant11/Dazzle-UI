a49ca353fc745fda19e19c3373c18a01cdfeeb68
Apurva Sawant <apurvasawant45@gmail.com>
2022-02-13 22:06:41 +0530

    add: grid and toast

--- js/component.js
+++ js/component.js
@@ -1,3 +1,5 @@
+// ||----------------------Modal JS------------------------
+
 const basicModalBtn = document.querySelector('.basic-modal-demo');
 const formModalBtn = document.querySelector('.form-modal-demo')
 const basicModal = document.querySelector('.basic-modal');
@@ -5,18 +7,64 @@ const formModal = document.querySelector('.form-modal');
 const basicModalCancel = document.querySelector('.basic-modal-cancel');
 const formModalClose = document.querySelector('.form-modal-close')
 
-basicModalBtn.addEventListener('click', () => {
-    basicModal.classList.add('show')
-})
 
-basicModalCancel.addEventListener('click', () => {
-    basicModal.classList.remove('show')
-})
+const toggleShowClass = (element) => {
+    element.classList.toggle('show');
+}
 
-formModalBtn.addEventListener('click', () => {
-    formModal.classList.add('show')
-})
+if(basicModalBtn){
+    basicModalBtn.addEventListener('click', () => {
+        toggleShowClass(basicModal);
+    })
+}
 
-formModalClose.addEventListener('click', () => {
-    formModal.classList.remove('show')
-})
+if(basicModalCancel){
+    basicModalCancel.addEventListener('click', () => {
+        toggleShowClass(basicModal)
+    })
+}
+
+if(formModalBtn){
+    formModalBtn.addEventListener('click', () => {
+        toggleShowClass(formModal)
+    })
+}   
+
+if(formModalClose){
+    formModalClose.addEventListener('click', () => {
+        toggleShowClass(formModal)
+    })
+}
+
+
+// ||----------- Toast JS --------------
+
+const simpleToastDemo = document.querySelector('.simple-toast-demo');
+const simpleToast = document.querySelector(".simple-toast-container");
+const simpleToastClose = document.querySelector('.simple-toast-close');
+
+const iconToastDemo = document.querySelector('.icon-toast-demo');
+const iconToast = document.querySelector(".icon-toast-container");
+const iconToastClose = document.querySelector('.icon-toast-close');
+
+if(simpleToastDemo) {
+    simpleToastDemo.addEventListener('click', () => {
+        toggleShowClass(simpleToast)
+        setTimeout(() => toggleShowClass(simpleToast), 3000);
+    });
+}
+
+if(simpleToastClose) {
+    simpleToastClose.addEventListener('click', () => toggleShowClass(simpleToast))
+}
+
+if(iconToastDemo) {
+    iconToastDemo.addEventListener('click', () => {
+        toggleShowClass(iconToast)
+        setTimeout(() => toggleShowClass(iconToast), 3000);
+    });
+}
+
+if(iconToastClose) {
+    iconToastClose.addEventListener('click', () => toggleShowClass(iconToast))
+}
